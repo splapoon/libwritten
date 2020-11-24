eee985c13ae5fb452f11ea49de0cad8652852e6d
mech <70609400+splapoon@users.noreply.github.com>
2020-11-24 12:57:42 -0500

    add libwritten header

---
+++ code/libwritten.h
@@ -0,0 +1,16 @@
+#ifndef WRITE_H_   /* Include guard */
+#define WRITE_H_
+#include <stdio.h>
+//#include
+
+int printred(int isBold, int isNewl);
+int printgreen(int isBold, int isNewl);
+int printyellow(int isBold, int isNewl);
+int printblue(int isBold, int isNewl);
+int printmagneta(int isBold, int isNewl);
+int printcyan(int isBold, int isNewl);
+int printreset(int isNewl);
+void night(int delay);
+void cbycprint(char *toWrite, int speed);
+void opentxt(char *name, int shouldCBYC, int speed);
+#endif
